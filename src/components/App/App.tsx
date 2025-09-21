3684fba4321787ad7c629436c906d2acff5d6961
unknown <googfather8@gmail.com>
2025-09-21 12:01:24 +0300

    moved the src/services folders

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -12,8 +12,8 @@ import Loader from "../Loader/Loader";
 import ErrorMessage from "../ErrorMessage/ErrorMessage";
 
 import type { Note } from "../types/note";
-import { fetchNotes, createNote, deleteNote } from "../services/noteService";
-import type { FetchNotes, CreateNote } from "../services/noteService";
+import { fetchNotes, createNote, deleteNote } from "../../services/noteService";
+import type { FetchNotes, CreateNote } from "../../services/noteService";
 
 import styles from "./App.module.css";
 
